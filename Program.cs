7531e0e31e323f7c992d53f8ef5e3128c618ef0a
PopovAzebali <106154882+PopovAzebali@users.noreply.github.com>
2022-05-26 15:37:21 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName = "Попов";
+            string lastName = "Алексей";
+            string reserve;
+
+            Console.WriteLine("До исправления");
+            Console.WriteLine("Имя : " + firstName);
+            Console.WriteLine("Фамилия : " + lastName);
+            reserve = firstName;
+            firstName = lastName;
+            lastName = reserve;
+            Console.WriteLine("После исправления");
+            Console.WriteLine("Имя : " + firstName);
+            Console.WriteLine("Фамилия : " + lastName);
+        }
+    }
+}
